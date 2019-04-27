4b188082b2b884697fcb8a47964af5ca2c369662
amitnagmode <amitnagmode@gmail.com>
2019-04-27 18:57:21 +0100

    lambda and s3 bucket initial stab

---
+++ main.tf
@@ -0,0 +1,79 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# iam
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs_role_policy_attach" {
+  role       = "${aws_iam_role.iam_for_lambda.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
+# lambda
+provider "archive" {}
+
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "hello_lambda.py"
+  output_path = "hello_lambda.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "hello_lambda"
+
+  filename         = "${data.archive_file.zip.output_path}"
+  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
+
+  role    = "${aws_iam_role.iam_for_lambda.arn}"
+  handler = "hello_lambda.lambda_handler"
+  runtime = "python3.6"
+
+  environment {
+    variables = {
+      greeting = "Hello amit"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "0371-amit-name"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+
+  lambda_function {
+    lambda_function_arn = "${aws_lambda_function.lambda.arn}"
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".txt"
+  }
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda.arn}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${aws_s3_bucket.bucket.arn}"
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "eu-west-2"
+}
+
+
+
