ccffc6132f1788bcbc71853bba2a5452f8b30318
amitnagmode <amitnagmode@gmail.com>
2019-04-28 14:26:05 +0100

    returning the file name and printing it

--- s3_trigger_lambda.py
+++ s3_trigger_lambda.py
@@ -4,8 +4,8 @@ ENV = os.environ.get('environment')
 
 
 def lambda_handler(event, context):
-    print('This print is form lambda')
     file_name = event['Records'][0]['s3']['object'].get('key')
+    print(f"{file_name} is added to s3 on {ENV}")
     return f"{file_name} is added to s3 on {ENV}"
 
 
